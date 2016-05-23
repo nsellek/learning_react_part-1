4373ca032e6cc1d27fc47ebd2d6de8c145409ecd
nsellek <nick@seansellek.com>
2016-05-23 14:47:29 -0400

    created a form to allow input of store name

--- scripts/main.js
+++ scripts/main.js
@@ -7,13 +7,17 @@ var ReactDOM = require('react-dom');
 */
 
 var StorePicker = React.createClass({
-  
+
   render : function(){
     return (
-      <p>hello</p>  
+      <form className="store-selector">
+        <h2> Please Enter A Store </h2>
+        <input type="text" ref='storeId' required />
+        <input type="Submit" />
+      </form>
     )
   }
-  
+
 });
 
 ReactDOM.render(<StorePicker/>, document.querySelector("#main"));
